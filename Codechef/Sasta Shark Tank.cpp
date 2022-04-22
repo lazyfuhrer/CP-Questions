6ee9741f9b9c37e55e611d1f1261cd591b0935a7
Biswarghya Biswas <64888892+lazyfuhrer@users.noreply.github.com>
2022-04-22 23:54:17 +0530

    SST

---
+++ Codechef/Sasta Shark Tank.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int a,b;
+        cin >> a >> b;
+        if (a*10 > b*5)
+            cout << "FIRST"<< endl;
+        else if (a*10 < b*5)
+            cout << "SECOND" << endl;
+        else cout << "ANY" << endl;
+    }
+}
